9d4687b0d06aac6692c329151f6235b66e20cc8b
christian otkjær <christian.otkjaer@gmail.com>
2016-12-09 09:16:23 +0100

    Initial Commit

---
+++ Clock/Clock.h
@@ -0,0 +1,19 @@
+//
+//  Clock.h
+//  Clock
+//
+//  Created by Christian Otkjær on 09/12/16.
+//  Copyright © 2016 Silverback IT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Clock.
+FOUNDATION_EXPORT double ClockVersionNumber;
+
+//! Project version string for Clock.
+FOUNDATION_EXPORT const unsigned char ClockVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Clock/PublicHeader.h>
+
+
